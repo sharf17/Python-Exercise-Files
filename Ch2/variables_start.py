58fd640714d58fefcf9089dbb6066109935e3150
sharf17 <mohammedsharfuddin390@gmail.com>
2021-07-13 23:55:07 +0530

    Updated

--- Ch2/variables_start.py
+++ Ch2/variables_start.py
@@ -3,17 +3,22 @@
 #
 
 # Declare a variable and initialize it
-
-
+a = 10
+print(a)
 
 # re-declaring the variable works
-
-
+a = 'sharf'
+print(a)
 
 # ERROR: variables of different types cannot be combined
-
-
-
+#a,b = 'sharf', 10
+#c = a+b
+#print(c)
 # Global vs. local variables in functions
-
-
+a = 10
+def stat():
+    print('Hola amigos')
+    a = 20
+    print(a)
+stat()
+print(a)
